e2d89032a36b4453c647b3e30c6ca962600882a7
Isabel Bou <isabelbouaguiar@gmail.com>
2023-09-30 18:46:15 +0200

    feat: Code refactor

---
+++ backend/src/orm/storeData.js
@@ -0,0 +1,9 @@
+module.exports.saveCard = (card) => {
+    // TODO: Store the basic story card data on the database
+}
+
+
+
+module.exports.saveEffect = (effect) => {
+    // TODO: Store the basic effect list on the database
+}
