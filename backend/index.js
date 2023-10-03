b27f30a6bd0e37276810ce18faa1be98269f56f9
Isabel Bou <isabelbouaguiar@gmail.com>
2023-10-03 15:21:01 +0200

    feat: Working draft of the migration to SQLite

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,7 @@
 
 const { basicEffectExtraction, basicCardExtraction  } = require('./src/clients/dataExtraction');
+const { runMigrations } = require('./src/orm/migration');
 
+runMigrations();
 basicEffectExtraction();
 basicCardExtraction();
